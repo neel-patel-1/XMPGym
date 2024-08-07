cb3327891f5b6b4409b6e03cc4cf11dbf52e75ac
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-08-06 20:14:38 -0500

    very few exotic users, change per-request allocation style on a per-request basis

--- three_phase_composable.cpp
+++ three_phase_composable.cpp
@@ -718,6 +718,60 @@ void axcore_axcore_allocator(
   *offload_args = off_args;
 }
 
+void axcore_compress_aecs_axcore_allocator(
+  int total_requests,
+  int initial_payload_size,
+  int max_axfunc_output_size,
+  int max_post_proc_output_size,
+  void input_generator(int payload_size, void **p_msgbuf, int *outsize),
+  timed_offload_request_args ***offload_args,
+  ax_comp *comps, uint64_t *ts0,
+  uint64_t *ts1, uint64_t *ts2,
+  uint64_t *ts3, uint64_t *ts4
+)
+{
+  timed_offload_request_args **off_args =
+    (timed_offload_request_args **)malloc(total_requests * sizeof(timed_offload_request_args *));
+
+  int max_ax_func_1_output_size;
+  int expected_ax_func_1_output_size = initial_payload_size;
+
+  for(int i = 0; i < total_requests; i++){
+    off_args[i] = (timed_offload_request_args *)malloc(sizeof(timed_offload_request_args));
+
+    input_generator(initial_payload_size,
+      &(off_args[i]->pre_proc_input), &(off_args[i]->pre_proc_input_size)); /* ax func 1 inp */
+    /* write prefault */
+    // write_prefault(off_args[i]->pre_proc_input, initial_payload_size);
+
+    /* ax func 1 outp */
+    max_ax_func_1_output_size = get_compress_bound(initial_payload_size); /* in case compress */
+    off_args[i]->pre_proc_output = (void *)aligned_alloc(4096, max_ax_func_1_output_size);
+    /* write prefault */
+    write_prefault(off_args[i]->pre_proc_output, max_ax_func_1_output_size);
+
+    /* ax func 2 outp */
+    off_args[i]->ax_func_input_size = initial_payload_size; /* only user is decompress and is therefore equal */
+    off_args[i]->ax_func_output = (void *)aligned_alloc(4096, max_axfunc_output_size);
+    /*write prefault */
+    write_prefault(off_args[i]->ax_func_output, max_axfunc_output_size);
+
+    off_args[i]->comp = &comps[i];
+
+    off_args[i]->ts0 = ts0;
+    off_args[i]->ts1 = ts1;
+    off_args[i]->ts2 = ts2;
+    off_args[i]->ts3 = ts3;
+    off_args[i]->ts4 = ts4;
+    off_args[i]->id = i;
+    off_args[i]->desc = (struct hw_desc *)malloc(sizeof(struct hw_desc));
+    off_args[i]->aecs =
+      (aecs_ptr_t *)aligned_alloc(32, IAA_COMPRESS_SRC2_SIZE);
+  }
+  *offload_args = off_args;
+}
+
+
 void free_axcore_axcore(
   int total_requests,
   timed_offload_request_args ***off_args
