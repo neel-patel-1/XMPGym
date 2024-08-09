7e2c81cc0f344524b78b343c4e87ec5b2c9f3a06
Neel Patel <n869p538@sapphire.ittc.ku.edu>
2024-08-08 23:59:50 -0500

    parse and run e2e brkdown

---
+++ scripts/parse_deser_decomp_hash.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+source configs/phys_core.sh
+source configs/query_size.sh
+
+[ -z "$CORE" ] && echo "CORE is not set" && exit 1
+[ -z "$query_size" ] && echo "query_size is not set" && exit 1
+
+[ -z "$QUERY_SIZE_LG" ] && echo "QUERY_SIZE_LG is not set" && exit 1
+
+
+for i in "${QUERY_SIZE_LG[@]}"
+do
+  query_size=$(( 2 ** $i ))
+  iters=5
+  reqs=10
+
+  echo -n "$query_size "
+  grep -v main three_phase_composable_logs/deser_decomp_hash_querysize_${query_size}.log \
+    | grep -v info \
+    | grep -v RPS \
+    | awk "\
+      /PreProcFunc Mean/{printf(\" %s \", \$5 );} \
+      /OffloadTax Mean/{printf(\"%s \", \$5);  } \
+      /AxFunc Mean/{printf(\"%s \", \$5);} \
+      /PostProcFunc Mean/{printf(\"%s\n\", \$5);} \
+      "
+done
+# [ info] alloc wq 0 shared size 128 addr 0x7f46d3d28000 batch sz 0x400 xfer sz 0x80000000
+# PreProcFunc Mean: 2559 Median: 2375 Stddev: 593.278181
+# OffloadTax Mean: 63 Median: 63 Stddev: 2.236068
+# AxFunc Mean: 51514 Median: 51715 Stddev: 447.644949
+# PostProcFunc Mean: 50803 Median: 49251 Stddev: 3118.376501
+# RPS Mean: 20426.598419 Median: 20424.425793 Stddev: 9857526.115615
+# PreProcFunc Mean: 2255 Median: 2248 Stddev: 37.523326
+# OffloadTax Mean: 616 Median: 616 Stddev: 2.645751
+# AxFunc Mean: 14044 Median: 14026 Stddev: 49.295030
+# PostProcFunc Mean: 49511 Median: 49516 Stddev: 53.469618
+# RPS Mean: 31324.615047 Median: 31432.821146 Stddev: 2641916.884308
+# PreProcFunc Mean: 2313 Median: 2311 Stddev: 39.408121
+# OffloadTax Mean: 571 Median: 571 Stddev: 2.449490
+# AxFunc Mean: 122604 Median: 122587 Stddev: 1896.719800
+# PostProcFunc Mean: 49812 Median: 49838 Stddev: 119.256027
+# RPS Mean: 39541.822501 Median: 39537.248513 Stddev: 31120437.404001
---
+++ scripts/run_deser_decomp_hash.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# sudo python3 scripts/accel_conf.py --load=configs/iaa-1n1d8e1w128q-s-n2.conf
+
+source configs/phys_core.sh
+source configs/devid.sh
+source configs/query_size.sh
+
+[ -z "$CORE" ] && echo "CORE is not set" && exit 1
+[ -z "$query_size" ] && echo "query_size is not set" && exit 1
+
+iters=5
+reqs=10
+
+make -j CXXFLAGS="-DPERF"
+mkdir -p three_phase_composable_logs
+
+for i in "${QUERY_SIZE_LG[@]}"
+do
+  query_size=$(( 2 ** $i ))
+  iters=5
+  reqs=10
+
+  make -j CXXFLAGS="-DPERF"
+  mkdir -p three_phase_composable_logs
+
+  taskset -c $CORE sudo LD_LIBRARY_PATH=/opt/intel/oneapi/ippcp/2021.11/lib \
+    ./three_phase_composable \
+    -t $reqs -i $iters \
+    -s ${query_size} \
+    -m $DSA_DEV_ID -n $IAA_DEV_ID \
+    -k 0 \
+    | tee three_phase_composable_logs/deser_decomp_hash_querysize_${query_size}.log
+done
